629acde3bcfaa28a3fb3f542a5140b501addc301
Eugene Wekesa <eugenco578@gmail.com>
2026-05-19 15:05:27 +0300

    feat: update hero image source in ContactPage to external URL

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -186,7 +186,7 @@ export function ContactPage() {
       {/* Hero Section */}
       <section className="relative h-96 overflow-hidden">
         <img
-          src="/assets/hero.png"
+          src="https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=800"
           alt="Contact us"
           className="w-full h-full object-cover"
         />
